a64e58d6b957311dfb857279bc53705eca96d92b
akashjv02 <akashjv02@gmail.com>
2025-11-06 23:07:03 +0530

    Add basic calculator functionality with operator selection

---
+++ Mechine test-4.py
@@ -0,0 +1,12 @@
+num1 = (input("Enter first number: "))
+num2 = (input("Enter second number: "))
+
+print("Select operators:\n"
+      "+\n"
+      "-\n"
+      "*\n"
+      "/\n"
+      "%\n")
+operators = input("Enter operator: ")
+
+        
