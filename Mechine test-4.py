9c3b3120957cc05fa12c71fd8e0d1a4c690715b7
akashjv02 <akashjv02@gmail.com>
2025-11-07 00:15:09 +0530

    Refactor calculator code to ensure proper input handling and add operator functionality

--- Mechine test-4.py
+++ Mechine test-4.py
@@ -1,12 +1,40 @@
-num1 = (input("Enter first number: "))
-num2 = (input("Enter second number: "))
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
 
 print("Select operators:\n"
       "+\n"
       "-\n"
       "*\n"
-      "/\n"
-      "%\n")
+      "/\n")
 operators = input("Enter operator: ")
 
-        
+class Calculator:
+    def add(self, num1, num2):
+        return num1 + num2
+
+    def sub(self, num1, num2):
+        return num1 - num2
+
+    def mul(self, num1, num2):
+        return num1 * num2
+
+    def div(self, num1, num2):
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Cant devide by 0"  
+
+calc = Calculator()  
+
+
+if operators == "+":
+    print("Result :", calc.add(num1, num2))
+    
+elif operators == "-":
+    print("Result :", calc.sub(num1,  num2))  
+    
+elif operators == "*":
+    print("Result :", calc.mul(num1, num2))
+
+elif operators == "/":
+    print("Result :", calc.div(num1, num2))      
