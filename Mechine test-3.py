920048dfa90341aa5210fb1b9581f7182f9525cf
akashjv02 <akashjv02@gmail.com>
2025-11-06 21:43:52 +0530

    Add student lookup functionality by user ID

---
+++ Mechine test-3.py
@@ -0,0 +1,10 @@
+student = [
+    {"id": 1, "name": "rajesh"}, 
+    {"id": 2, "name": "rahul"}, 
+    {"id": 3, "name": "sruthi"}]
+
+u_num = int(input("Enter User ID: "))
+
+for x in student:
+    if x["id"] == u_num:
+        print("Student Name is:", student["name"], "ID is :", student["id"])
